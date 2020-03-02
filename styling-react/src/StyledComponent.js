2643517000116e2b897e5cf1944a6065fb26c240
0pippi0 <j.romany91@gmail.com>
2020-03-03 00:28:09 +0900

    media 를 StyledComponent.js 에서 만들어주긴 했지만, 실제로 사용한다면 아예 다로 다른 파일로 분리시켜서 여기저기서 불러와서 사용하는게 훨씬 편할 것
    styled-components 는 정말 유용한 라이브러리
    서버 사이드렌더링 도 지원

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -6,22 +6,33 @@ import styled, { css } from 'styled-components';
 css 를 불러와야합니다. 
 */
 
+const sizes = {
+    desktop: 1024,
+    tablet: 768
+};
+
+// 위에있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어줍니다.
+// 참고: https://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css `
+        @media (max-width: ${sizes[label] / 16}em) {
+            ${css(...args)};
+        }
+    `;
+
+    return acc;
+}, {});
+
 const Box = styled.div`
     /* props 로 넣어준 값을 직접 전달해줄 수 있습니다. */
     background: ${props => props.color || 'blue'};
     padding: 1rem;
     display: flex;
-    /* 기본적으로는 1024px 에 가운데 정렬을 하고
-    가로 크기가 작아짐에 따라 사이즈를 줄이고
-    768px 미만으로 되면 꽉 채웁니다 */
     width: 1024px;
     margin: 0 auto;
-    @media (max-width: 1024px) {
-        width: 768px;
-    }
-    @media (max-width: 768px) {
-        width: 100%;
-    }
+    ${media.desktop`width: 768px;`}
+    ${media.tablet`width: 768px;`};
+
 `;
 
 const Button = styled.button`
